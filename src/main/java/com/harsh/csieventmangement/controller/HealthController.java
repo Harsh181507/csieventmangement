ad15d81e42db3409882506e2a43dbdb0bca3faba
Kumar Harsh Raj <harshcodes.sshhr@gmail.com>
2026-02-15 23:08:23 +0530

    Fixed Issue for Leader in Team

---
+++ src/main/java/com/harsh/csieventmangement/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.harsh.csieventmangement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "CSI Event Management Backend Running 🚀";
+    }
+}
